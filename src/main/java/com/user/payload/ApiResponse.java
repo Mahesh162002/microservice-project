c886b80116c6d4d4a10bc2c4a8d0021324ee2e88
Mahesh162002 <sutarmaheshgambhir@gmail.com>
2025-04-11 17:18:17 +0530

    Update ApiResponse.java
    Secound commit

--- src/main/java/com/user/payload/ApiResponse.java
+++ src/main/java/com/user/payload/ApiResponse.java
@@ -11,6 +11,7 @@ public class ApiResponse {
     private String message;
     private boolean success;
     private HttpStatus status;
+    private String mobile;
 
     public String getMessage() {
         return message;
